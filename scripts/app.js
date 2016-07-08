a2f24ad11964e1abec4dc232017f67ee81642550
chubwa <tuzoengelbert@gmail.com>
2016-07-08 18:44:33 +0300

    adding locale

---
+++ scripts/app.js
@@ -0,0 +1,45 @@
+'use strict';
+
+/* App Module */
+
+var systemSetting = angular.module('systemSetting',
+                    ['ui.bootstrap',
+                      'ui.bootstrap.contextMenu',
+                     'ngRoute', 
+                     'ngCookies', 
+                     'ngSanitize',
+                     'systemSettingDirectives',
+                     'systemSettingControllers',
+                     'systemSettingServices',
+                     'systemSettingFilters',
+                     'd2Directives',
+                     'd2Filters',
+                     'd2Services',
+                     'd2Controllers',
+                     'angularLocalStorage', 
+                     'pascalprecht.translate',
+                     'd2HeaderBar',
+                      'ngCsv',
+                     'sticky'])
+              
+.value('DHIS2URL', '../../..')
+
+    .config(function($httpProvider, $routeProvider, $translateProvider) {
+
+        $httpProvider.defaults.useXDomain = true;
+        delete $httpProvider.defaults.headers.common['X-Requested-With'];
+
+        $routeProvider.when('/', {
+            templateUrl:'views/general.html',
+            controller: 'GeneralController'
+        }).when('/Generals',{
+            templateUrl:'views/general.html',
+            controller: 'GeneralController'
+        }).otherwise({
+            redirectTo : '/'
+        });
+
+        $translateProvider.preferredLanguage('en');
+        $translateProvider.useSanitizeValueStrategy('escaped');
+        $translateProvider.useLoader('i18nLoader');
+    });
